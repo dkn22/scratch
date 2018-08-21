6f16e812142f1926f6fffcb93fc2e3fae61414fd
dkn22 <nguyendatkhac@gmail.com>
2018-08-21 23:52:17 +0100

    added max heaps
    '
    '

--- cs/heaps.py
+++ cs/heaps.py
@@ -143,3 +143,74 @@ class PriorityQueue:
 
     def __contains__(self, val):
         return len([tup for tup in self.heaplist if tup[1] == val]) > 0
+
+
+class MaxBinHeap:
+    def __init__(self):
+        self.heaplist = [0]
+        self.current_size = 0
+
+    def __repr__(self):
+        params = {'heaplist': self.heaplist,
+                  'current_size': self.current_size
+                  }
+
+        return '%s(%r)' % (self.__class__.__name__, params)
+
+    def perc_up(self, i):
+        while i // 2 > 0: # while has 'parent'
+            if self.heaplist[i] > self.heaplist[i // 2]:
+                self.heaplist[i // 2], self.heaplist[i] =\
+                    self.heaplist[i], self.heaplist[i // 2]
+
+            i = i // 2
+
+    def insert(self, value):
+        self.heaplist.append(value)
+        self.current_size += 1
+        self.perc_up(self.current_size)
+
+    def find_max(self):
+        return self.heaplist[1]
+
+    def max_child(self, i):
+        if i * 2 + 1 > self.current_size:
+            return i * 2
+
+        else:
+            if self.heaplist[i * 2] > self.heaplist[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def perc_down(self, i):
+        while (i * 2) <= self.current_size:
+            mc = self.max_child(i)
+            if self.heaplist[i] < self.heaplist[mc]:
+                self.heaplist[i], self.heaplist[mc] =\
+                    self.heaplist[mc], self.heaplist[i]
+
+            i = mc
+
+    def del_max(self):
+        root = self.find_max()
+
+        self.heaplist[1] = self.heaplist[self.current_size]
+        self.current_size -= 1
+        self.heaplist.pop()
+        self.perc_down(1)
+
+        return root
+
+    @classmethod
+    def build_heap(cls, alist):
+        heap = MaxBinHeap()
+        heap.heaplist = [0] + alist[:]
+        heap.current_size = len(alist)
+        i = heap.current_size // 2
+
+        while i > 0:
+            heap.perc_down(i)
+            i -= 1
+
+        return heap
